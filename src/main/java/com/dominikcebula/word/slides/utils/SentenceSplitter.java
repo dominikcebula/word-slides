67266202ba845c485936836cc46ccd6376b715d5
Dominik Cebula <dominikcebula@gmail.com>
2024-05-14 22:09:48 +0200

    Code readability update

--- src/main/java/com/dominikcebula/word/slides/utils/SentenceSplitter.java
+++ src/main/java/com/dominikcebula/word/slides/utils/SentenceSplitter.java
@@ -1,13 +1,14 @@
 package com.dominikcebula.word.slides.utils;
 
+import static org.apache.commons.lang3.StringUtils.isBlank;
+
 public class SentenceSplitter {
 
     public static String[] splitSentence(String sentence) {
-        var splits = sentence.split(" +");
-
-        if (splits.length == 1 && splits[0].isEmpty())
+        if (!isBlank(sentence)) {
+            return sentence.split(" +");
+        } else {
             return new String[0];
-        else
-            return splits;
+        }
     }
 }
